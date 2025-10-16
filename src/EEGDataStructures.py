562ac70dd04c8d165b6c455dd528d1a988242a21
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-10-16 08:45:21 -0500

    Added user_functions.py, EEGDataStructures.py

---
+++ src/EEGDataStructures.py
@@ -0,0 +1,155 @@
+import numpy as np
+from numpy import typing as npt
+
+from pymatreader import read_mat
+
+from typing import *
+
+# NOTE: This implementation was made for single-channel data. 
+class EEGTrial:
+    """
+    Represents data from a single trial: data, timestamps, label, and trial index.
+    """
+    def __init__(self, data: npt.NDArray, timestamps: npt.NDArray, label: int, trial_index: int):
+        """
+        :param data: A 1D numpy array containing the data.
+        :param timestamps: The clock time corresponding to each datapoint in `data`
+        :param label: This `EEGTrial`'s label
+        :param trial_index: This `EEGTrial`'s trial index
+        """
+        self.data = self.formatted_data(data)
+        self.timestamps = timestamps
+        self.label = label
+        self.trial_index = trial_index
+
+    @staticmethod
+    def formatted_data(data: npt.NDArray) -> npt.NDArray:
+        """
+        Raises an error if `data` is not 1D, and returns it otherwise.
+        """
+        if data.ndim != 1:
+            raise ValueError("Only 1D arrays are permitted.")
+        return data
+    
+    @staticmethod
+    def formatted_timestamps(timestamps: npt.NDArray) -> npt.NDArray:
+        """
+        Raises an error if `data` is not 1D, and returns it otherwise.
+        """
+        if timestamps.ndim != 1:
+            raise ValueError("Only 1D arrays are permitted.")
+        return timestamps
+    
+    def trim(self, start_time=0, end_time=None):
+        """
+        Binary search the timestamps for the start and end time
+        """
+        pass
+
+class EEGData: 
+    def __init__(self, trials: Sequence[EEGTrial]):
+        self.all_data = trials
+
+class AveragedEEGTrial:
+    """
+    Represents the averaged data of multiple EEGTrial instances.
+    """
+    def __init__(self, trials: Sequence[EEGTrial], trial_index: int):
+        stack = np.stack([trial.data for trial in trials])
+        averaged_data = stack.mean(axis=0)
+        self.data = averaged_data
+        self.times = trials[0].times
+        self.label = trials[0].label
+        self.trial_index = trial_index
+
+class EEGData: 
+    """
+    Represents all EEGTrial instances obtained from a subject. 
+    """
+    # def __init__(self, identifier: str, trials: Sequence[EEGTrial]):
+    #     """
+    #     Initialize from a sequence  of EEGTrial instances.
+    #     """
+    #     self.identifier = identifier 
+    #     self.trials = trials
+
+    def __init__(self, *, identifier: str, filename: str=None, trials: Sequence[EEGTrial]=None):
+        """
+        Initialize directly from the filename.
+        """
+        # Check preconditions
+        if not filename and not trials:
+            print("Error: Either filename or trials must not be None.")
+            return
+        elif filename and trials:
+            print("Error: filename and trials cannot both be non-None.")
+            return
+        
+        if trials:
+            self.trials = trials
+        elif filename:
+            self.trials = [] # Initialize to empty list 
+            data, times, labels = self.extract_from_file(filename)
+
+            for trial_index in range(len(data)):
+                trial = data[trial_index]
+                self.trials.append(EEGTrial(data=trial, 
+                                            times=times, 
+                                            label=labels[trial_index], 
+                                            trial_index=trial_index))
+
+        self.identifier = identifier
+            
+    def extract_from_file(self, filename, data_extractor=None, times_extractor=None, labels_extractor=None):
+        """
+        Returns the data, times, and labels using the given filename. The structure of returned data
+        is found in the documentation for the `__array__` method. 
+        """
+        raw_data = read_mat(filename)
+
+        data = raw_data["ffr_nodss"] if not data_extractor else data_extractor(raw_data)
+        times = raw_data["time"] if not times_extractor else times_extractor(raw_data)
+        labels = raw_data["#subsystem#"]["MCOS"][3] if not labels_extractor else labels_extractor(raw_data)
+
+        return data, times, labels
+
+    def __array__(self):
+        """
+        Converts the array of trials into a numpy array.
+        We have individual trials along the 1st dimension,
+            channels along the 2nd dimension,
+            and datapoints for the channel of the trial along the 3rd dimension.
+
+            arr[a][b][c] is the c-th datapoint for the b-th channel for the a-th trial
+        """
+        return np.stack([trial.data for trial in self.trials])
+    
+    def to_subaveraged(self, n):
+        """
+        Returns a subaveraged vertion of this EEGData instance. 
+        """
+        grouped_trials = {}
+        for trial in self.trials:
+            trial_label = trial.label
+            if trial_label in grouped_trials:
+                grouped_trials[trial_label].append(trial)
+            else:
+                grouped_trials[trial_label] = [trial]
+        
+        sub_averages = []
+        for _, trial_subgroup in grouped_trials.items():
+            for i in range(0, len(trial_subgroup), n):
+                group = trial_subgroup[i:i+n]
+                sub_averages.append(AveragedEEGTrial(trials=group, trial_index=len(sub_averages)))
+        
+        return EEGData(identifier="test", trials=sub_averages)
+
+
+if __name__ == "__main__":
+    filename = "../../../trial-classification/data/4T1002.mat"
+    something = EEGData(identifier="test", filename=filename)
+    print(np.array(something)[0][0])
+
+
+
+    
