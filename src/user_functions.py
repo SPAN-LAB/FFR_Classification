06c3a9d01f04e4f839409221742ecd4753580e2d
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-10-16 10:29:49 -0500

    Completed EEGDataStructures.py

---
+++ src/user_functions.py
@@ -0,0 +1,48 @@
+def GLOBAL_map_class_labels():
+    """
+    TODO Kevin
+    EEGTrial Method Wrapper
+    """
+    return
+
+def GLOBAL_trim_ffr():
+    """
+    TODO Kevin
+    EEGTrial Method Wrapper
+    """
+    return
+
+def GLOABL_sub_average_data():
+    """
+    TODO Kevin
+    EEGSubject Method Wrapper
+    """
+    return
+
+def GLOBAL_test_split_stratified():
+    """
+    TODO Kevin
+    EEGSubject Method Wrapper
+    """
+    return
+
+def GLOBAL_k_fold_stratified():
+    """
+    TODO Kevin
+    EEGSubject Method Wrapper
+    """
+    return
+
+def GLOBAL_inference_model():
+    """
+    TODO Anu
+    Standalone function for inferencing on saved ONNX models.
+    """
+    return
+
+def GLOBAL_train_model():
+    """
+    TODO Anu
+    Standalone function for training predefined PyTorch models.
+    """
+    return
