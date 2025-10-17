64b0c1f61b086cbf4c0b2ba07fbc84a4ad5f22d3
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-10-17 15:34:58 -0500

    Changes

--- src/user_functions.py
+++ src/user_functions.py
@@ -1,37 +1,39 @@
-def GLOBAL_map_class_labels():
+from EEGDataStructures import EEGSubject
+
+def GLOBAL_map_class_labels(e: EEGSubject, csv_filepath: str):
     """
     TODO Kevin
     EEGTrial Method Wrapper
     """
-    return
+    e.map_labels(csv_filepath)
 
-def GLOBAL_trim_ffr():
+def GLOBAL_trim_ffr(e: EEGSubject, start_index: int, end_index: int):
     """
     TODO Kevin
     EEGTrial Method Wrapper
     """
-    return
+    e.trim(start_index=start_index, end_index=end_index)
 
-def GLOABL_sub_average_data():
+def GLOABL_sub_average_data(e: EEGSubject, size: int):
     """
     TODO Kevin
     EEGSubject Method Wrapper
     """
-    return
+    e.subaverage(size=size)
 
-def GLOBAL_test_split_stratified():
+def GLOBAL_test_split_stratified(e: EEGSubject, ratio: float):
     """
     TODO Kevin
     EEGSubject Method Wrapper
     """
-    return
+    e.test_split(trials=e.trials, ratio=ratio)
 
-def GLOBAL_k_fold_stratified():
+def GLOBAL_k_fold_stratified(e: EEGSubject, num_folds: int):
     """
     TODO Kevin
     EEGSubject Method Wrapper
     """
-    return
+    e.stratified_folds(num_folds=num_folds)
 
 def GLOBAL_inference_model():
     """
