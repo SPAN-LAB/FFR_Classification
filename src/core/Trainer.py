ace94d122d0a295f989b539a4e5daf8731e2989b
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-10-29 15:29:40 -0500

    Fix GUI bugs; stable now

--- src/core/Trainer.py
+++ src/core/Trainer.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from EEGSubject import EEGSubjectInterface
+from .EEGSubject import EEGSubjectInterface
 
 from torch import nn
 
@@ -7,9 +7,12 @@ class TrainerInterface(ABC):
     model: nn.Module
 
     @abstractmethod
+    def set_device(self, device_preference: str): ...
+
+    @abstractmethod
     def train(self, subject: EEGSubjectInterface): ... 
 
     @abstractmethod
     def run(self, subject: EEGSubjectInterface): ... 
 
-class Trainer(TrainerInterface):
+# class Trainer(TrainerInterface):
