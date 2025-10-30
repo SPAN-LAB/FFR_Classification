0113027f2bce971cfa234bb78f214c16e1b62040
Anu-Shukla <shukla33@wisc.edu>
2025-10-29 20:50:17 -0500

    minute changes

--- src/core/Trainer.py
+++ src/core/Trainer.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from EEGSubject import EEGSubjectInterface
+from EEGSubject import EEGSubjectInterface, EEGSubject
 
 from torch import nn
 
@@ -7,9 +7,19 @@ class TrainerInterface(ABC):
     model: nn.Module
 
     @abstractmethod
-    def train(self, subject: EEGSubjectInterface): ... 
+    def train(self, subject: EEGSubjectInterface, model_name: str, 
+              num_epochs: int, lr: float, stopping_criteria: bool): ... 
 
     @abstractmethod
     def run(self, subject: EEGSubjectInterface): ... 
 
 class Trainer(TrainerInterface):
+    def __init__(self):
+        self.model = None
+        
+
+    def train(self, subject: EEGSubject):
+        return 1
+
+    def run(self, subject: EEGSubject):
+        return 1
