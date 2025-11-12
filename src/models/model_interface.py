ab3d7396e0d3d8f3ae4cd29672a8cfc4df2543fe
Anu-Shukla <shukla33@wisc.edu>
2025-11-11 22:18:25 -0600

    Added model interface

---
+++ src/models/model_interface.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from ..core.eeg_subject import EEGSubject
+from ..core.eeg_trial import EEGTrial
+from typing import Any
+
+
+class ModelInterface(ABC):
+    @abstractmethod
+    def __init__(self, subject: EEGSubject):
+        """
+        Initialize a model bound to exactly one subject
+        """
+        ...
+
+    @abstractmethod
+    def evaluate(self) -> float:
+        """
+        Evaluates the accuracy of the model using cross-validation in the following steps for the subject that was passed into this object
+        1. The subject data is split into folds (see 'EEGSubject.Fold' and ffr_prep.make_folds())
+        2. For `i` in 1 through n where n = number of folds:
+            - Let the test set be (`EEGSubject.trials`) - (trials in fold `i`)
+            - Let the train set be (`EEGSubject.trials`) - (trials NOT in fold `i`)
+            - Train the model on the train set's `EEGTrial` instances
+            - Use that model to create predictions on the test set's `EEGTrial` instances
+        3. After training on all folds, 1 prediction is made for every `EEGTrial` of the subject
+        4. We check each prediction against the actual label to obtain the accuracy of the model.
+        This is the accuracy of the model trained on this `EEGSubject`
+
+        :returns: Accuracy as a float for a model on one subject's data
+        """
+        ...
+
+    @abstractmethod
+    def train(self, output_path: str): ...
+
+    @abstractmethod
+    def infer(self, trials: list[EEGTrial]): ...
+
+
+class NeuralNetworkInterface(ModelInterface, ABC):
+    """
+    Extends ModelInerface to add neural network specific behavior
+    The following methods are what need to be defined in addition
+    to the methods specified in ModelInterface
+    """
+
+    model: Any | None  # NOTE: to be set by the build() method
+
+    @abstractmethod
+    def __init__(self, subject: EEGSubject, hyperparameters: dict[str, Any]):
+        """
+        Adds hyperparameters to constructor/
+        hyperparameters: dictionary that defines the hyperparameters the model will use (such as num of epochs, stopping criteria, learning rate, etc...)
+        """
+        ...
+
+    @abstractmethod
+    def build(self) -> None:
+        """
+        Construct the architecture of your model and store it in self.model
+        """
+        ...
