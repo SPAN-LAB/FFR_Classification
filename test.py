20ec685d1c495c93760e4594f3276b2ddb6aaae6
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-11-13 11:43:42 -0600

    Remove unused files

--- test.py
+++
@@ -1,5 +0,0 @@
-from src.core import EEGSubject
-
-subject1: EEGSubject = EEGSubject.init_from_filepath("data/4T1002.mat")
-subject1.trim_by_timestamp(start_time=5, end_time=6)
-print(subject1.trials[0].timestamps)
