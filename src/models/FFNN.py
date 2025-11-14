d2b45896ac7a402498aa3e354efd3b5189690c2d
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-11-14 13:55:13 -0600

    Clean up `FFNN`

--- src/models/FFNN.py
+++ src/models/FFNN.py
@@ -1,35 +1,47 @@
 from torch import nn
 from .utils import TorchNNBase
-from math import floor
 
+from math import floor
 
 class FFNN(TorchNNBase, nn.Module):
     def __init__(self, training_options: dict[str, any]):
+        """
+        NOTE: This method MUST be copied verbatim into every concrete PyTorch NN.
+        """
         TorchNNBase.__init__(self,training_options)
         nn.Module.__init__(self)
 
     def build(self):
-
-        # TODO hardcoded values ; remove later
+        # TODO hardcoded values; remove later
         input_size = len(self.subject.trials[0].data)
         num_classes = 4 
-        dropout_p = 0.5
+        """
+        This part should become:
+
+            input_size = subject.num_datapoints
+            output_size = subject.num_possible_labels
+
+        """
         ########################################
-        # nn.Module().__init__()
+
+        dropout_p = 0.5
+
+        # Calculate hidden layer sizes based on the input size so they gradually decrease
         hidden1 = floor(input_size / 2)  # ~400
         hidden2 = floor(hidden1 / 2)  # ~80
         hidden3 = floor(hidden2 / 2)  # ~40
 
+        # Model architecture
         self.model = nn.Sequential(
             nn.Linear(input_size, hidden1),
             nn.ReLU(),
-            # nn.Dropout(dropout_p),
+            nn.Dropout(dropout_p),
             nn.Linear(hidden1, hidden2),
             nn.ReLU(),
-            # nn.Dropout(dropout_p),
+            nn.Dropout(dropout_p),
             nn.Linear(hidden2, hidden3),
             nn.ReLU(),
-            # nn.Dropout(dropout_p),
+            nn.Dropout(dropout_p),
             nn.Linear(hidden3, num_classes),
         )
 
