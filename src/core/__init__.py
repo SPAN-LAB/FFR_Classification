c8a1e0c3bf1f5ec72b7db51e8d1b0019c8d50149
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-11-14 12:22:47 -0600

    Update `AnalysisPipeline` with a typealias `PipelineState` This is reflected in the `__init__.py`  file as well

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,6 +1,8 @@
 from .eeg_subject import EEGSubject, EEGSubjectInterface
 from .eeg_trial import EEGTrial, EEGTrialInterface
 from .ffr_prep import FFRPrep
+from .analysis_pipeline import AnalysisPipeline, PipelineState
+
 from .trainer import Trainer, TrainerInterface
 from .plots import Plots, PlotsInterface
 
