381ce67b749e68db6f277c73ddf5f2bced00cf5f
Anu-Shukla <shukla33@wisc.edu>
2025-11-16 16:28:30 -0600

    Modified structure of RNN.py

--- src/models/RNN.py
+++ src/models/RNN.py
@@ -3,17 +3,10 @@ import torch
 from .utils import TorchNNBase
 
 
-class RNN(TorchNNBase, nn.Module):
-    def __init__(self, training_options: dict[str, any]):
-        TorchNNBase.__init__(self, training_options)
-        nn.Module.__init__(self)
-
-    def build(self):
-        input_size = len(self.subject.trials[0].data)
-        num_classes = 4
-        hidden_size = 750
-
-        self.model = nn.LSTM(
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+        self.lstm = nn.LSTM(
             input_size=input_size,
             hidden_size=hidden_size,
             batch_first=True,
@@ -26,7 +19,7 @@ class RNN(TorchNNBase, nn.Module):
         if x.ndim == 2:
             x = x.unsqueeze(-1)
 
-        output, (hidden_state, cell_state) = self.model(x)
+        _, (hidden_state, _) = self.lstm(x)
         hidden_forward = hidden_state[-2]
         hidden_backward = hidden_state[-1]
         hidden = torch.cat([hidden_forward, hidden_backward], dim=1)
@@ -34,3 +27,17 @@ class RNN(TorchNNBase, nn.Module):
         hidden = self.dropout(hidden)
         logits = self.fc(hidden)
         return logits
+
+
+class RNN_model(TorchNNBase):
+    def __init__(self, training_options: dict[str, any]):
+        TorchNNBase.__init__(self, training_options)
+        self.build()
+
+    def build(self):
+        input_size = 1
+        num_classes = 4
+        hidden_size = 750
+        self.model = RNN(
+            input_size=input_size, hidden_size=hidden_size, num_classes=num_classes
+        )
