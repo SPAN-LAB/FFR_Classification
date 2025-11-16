646b636f5ce8d7ccb32f1107a6782bd37f69ce65
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-11-16 17:16:37 -0600

    Update demo.py

--- demo.py
+++ demo.py
@@ -21,7 +21,8 @@ p = (
         training_options={
             "num_epochs": 20,
             "batch_size": 64,
-            "learning_rate": 0.001
+            "learning_rate": 0.001,
+            "weight_decay": 0.1
         }    
     )
 )
