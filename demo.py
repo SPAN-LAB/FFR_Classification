be2ca98625cd25ec4c2eb9a1ce862910cb237c91
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-11-17 13:18:10 -0600

    Clean up demo.py

--- demo.py
+++ demo.py
@@ -1,14 +1,13 @@
 from src.core import AnalysisPipeline, PipelineState
 from src.core import EEGSubject
 
-from local.constants import *
-# Replace this with a variable ``ALL_PATH``, a string representing the path to the directory
-# containing the data. Example:
+# Replace this with a string variable representing the path to the directory containing the data. 
+# Example:
 # 
-#     ALL_PATH = "area51/martian_subject_42/eeg_data"
+#     PATH = "area51/martian_subject_42/eeg_data"
 #
-# Using the ``ALL_PATH`` variable is NOT required. If you use a different variable name,
-# update the variable name in the ``.load_subjects(...)`` line.
+# Swap this variable into the ``load_subjects`` method on line 18 below.
+from local.constants import *
 
 loading_result = PipelineState()
 trimming_result = PipelineState()
@@ -16,7 +15,7 @@ subaverage_and_fold_result = PipelineState()
 
 p = (
     AnalysisPipeline()
-    .load_subjects(filepath_list=GOOD_D_PATH)
+    .load_subjects(folder_path=ALL_PATH)
     .save(to=loading_result)
     .trim_by_timestamp(start_time=0, end_time=float("inf")) # Keep all starting from 0 ms
     .save(to=trimming_result)
