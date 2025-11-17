3ea48afa96387605deb7bc0eba9b3f7834b97e31
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-11-17 13:11:39 -0600

    Add function detail specifiers
    Acts as a parser so GUI knows how to display functions and their options

---
+++ src/core/utils/details.py
@@ -0,0 +1,54 @@
+from .function_detail import FunctionDetail as FD
+from .function_detail import ArgumentDetail as AD
+from .function_detail import Selection
+
+def detail(detail: FD):
+    def decorator(func):
+        func.detail = detail
+        return func
+    return decorator
+
+subaverage_detail = FD(
+    label="Subaverage Trials",
+    argument_details=[
+        AD(
+            label="Number of Trials", 
+            type=int, 
+            default_value=5, 
+            description="The number of trials to combine through subaveraging."
+        )
+    ],
+    description="Combines trials through subaveraging. This can help reduce noise in your data."
+)
+
+fold_detail = FD(
+    label="Split into Folds",
+    argument_details=[
+        AD(
+            label="Number of Folds",
+            type=int,
+            default_value=5,
+            description="The number of groups to split the subject's trials into."
+        )
+    ],
+    description="Divides each subject's trials into the number of groups (folds) provided."
+)
+
+def finder(self):
+    return {
+        "one": 1,
+        "two": 2,
+        "three": 3
+    }
+
+dummy_detail = FD(
+    label="Dummy",
+    argument_details=[
+        AD(
+            label="String",
+            type=Selection,
+            default_value=Selection(finder),
+            description="Something"
+        )
+    ]
+)
