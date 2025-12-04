ea3ca0a6f6ebc4df9297e920412acb04eaa80eb8
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-12-04 16:18:47 -0600

    Adding better print statements

--- src/printing/printing.py
+++ src/printing/printing.py
@@ -1,34 +1,46 @@
-import sys
+from sys import stdout
 
 class StationaryPrinter:
     is_locked: bool = False
-        
+ 
     @staticmethod
     def sprint(printable):
         if StationaryPrinter.is_locked:
             print(f"\r{printable}", end="")
-            sys.stdout.flush()
+            stdout.flush()
         else:
             print(f"{printable}")
     
-    @classmethod
-    def lock(cls):
-        cls.is_locked = True
+    @staticmethod
+    def lprint(printable):
+        if not StationaryPrinter.lock():
+            print()
+        StationaryPrinter.sprint(printable)
+    
+    @staticmethod
+    def ulprint(printable):
+        if StationaryPrinter.unlock(): # if changed from locked
+            print()
+        StationaryPrinter.sprint(printable)
     
     @classmethod
-    def unlock(cls):
+    def lock(cls) -> bool:
+        prev = cls.is_locked
+        cls.is_locked = True
+        return prev
+    
+    @classmethod
+    def unlock(cls) -> bool:
+        prev = cls.is_locked
         cls.is_locked = False
-        print()
+        return prev
 
 if __name__ == "__main__":
-    sprint = StationaryPrinter.sprint
-    lock = StationaryPrinter.lock
-    unlock = StationaryPrinter.unlock
+    ulprint = StationaryPrinter.ulprint
+    lprint = StationaryPrinter.lprint
 
     # Example usage
-    sprint("Hello world!")
-    lock()
-    for i in range(1, 10000001):
-        sprint(f"{i}")
-    unlock()
-    sprint("End")
+    ulprint("Hello")
+    for i in range(1, 100001):
+        lprint(f"{i}")
+    ulprint("End")
