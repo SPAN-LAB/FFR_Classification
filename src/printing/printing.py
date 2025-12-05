868ef87a0f9f5cd3625ec0b538545e37eac1de68
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-12-05 12:32:20 -0600

    Updated printing usage

--- src/printing/printing.py
+++ src/printing/printing.py
@@ -2,45 +2,82 @@ from sys import stdout
 
 class StationaryPrinter:
     is_locked: bool = False
- 
-    @staticmethod
-    def sprint(printable):
-        if StationaryPrinter.is_locked:
-            print(f"\r{printable}", end="")
-            stdout.flush()
-        else:
-            print(f"{printable}")
     
     @staticmethod
-    def lprint(printable):
-        if not StationaryPrinter.lock():
-            print()
-        StationaryPrinter.sprint(printable)
+    def printl(printable):
+        """
+        Moves the cursor to the beginning of the line it is current on, prints the printable, 
+        and does not create a new line afterward. 
+        
+        Use this print method consecutively to write text in-place instead of onto new lines.
+        
+        Parameters
+        ----------
+        printable
+            The content to be printed. This can be anything compatible with the generic `print`.
+        """
+        StationaryPrinter.lock()
+        print(f"\r{printable}", end="")
+        stdout.flush()
     
     @staticmethod
-    def ulprint(printable):
-        if StationaryPrinter.unlock(): # if changed from locked
-            print()
-        StationaryPrinter.sprint(printable)
+    def print(printable):
+        """
+        Prints onto a new line like a normal `print` statement. 
+        
+        Use this method in replacement of `print` from builtins for compatability with `printl`. 
+        
+        Parameters
+        ----------
+        printable
+            The content to be printed. This can be anything compatible with the generic `print`.
+        """
+        StationaryPrinter.unlock()
+        print(printable)
     
     @classmethod
     def lock(cls) -> bool:
-        prev = cls.is_locked
+        """
+        Locks the printer onto the current line and returns what the printer's locked status was 
+        before the execution of this method. 
+        
+        Returns
+        -------
+        True if the printer was unlocked at the instant this function was called; false otherwise.
+        """
+        was_locked = cls.is_locked
         cls.is_locked = True
-        return prev
+        return was_locked
     
     @classmethod
     def unlock(cls) -> bool:
-        prev = cls.is_locked
+        """
+        Unlocks printer from the current line and returns what the printer's locked status was 
+        before the execution of this method. 
+        
+        Creates a new line if the printer was previously locked, 
+        since `printl` does not create a newline character. 
+        
+        Returns
+        -------
+        True if the printer was unlocked at the instant this function was called; false otherwise.
+        """
+        was_locked = cls.is_locked
+        if was_locked:
+            print()
         cls.is_locked = False
-        return prev
+        return was_locked
 
 if __name__ == "__main__":
-    ulprint = StationaryPrinter.ulprint
-    lprint = StationaryPrinter.lprint
+    printl = StationaryPrinter.printl
+    print = StationaryPrinter.print
+    unlock = StationaryPrinter.unlock
 
     # Example usage
-    ulprint("Hello")
-    for i in range(1, 100001):
-        lprint(f"{i}")
-    ulprint("End")
+    print("Title")
+    for i in range(1, 1000000):
+        printl(f"First loop value: {i}")
+    unlock() # Separates locked sections
+    for i in range(1, 1000000):
+        printl(f"Second loop value: {i}")
+    print("Footer")
