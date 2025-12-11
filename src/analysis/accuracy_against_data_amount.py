a1680411a0b6c0269894c42175d4b79e62bdcf17
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2025-12-11 12:37:12 -0600

    Add option to defer the loading of subjects

--- src/analysis/accuracy_against_data_amount.py
+++ src/analysis/accuracy_against_data_amount.py
@@ -3,7 +3,7 @@ import pandas as pd
 from copy import deepcopy
 from random import sample
 
-from ..core import AnalysisPipeline, PipelineState
+from ..core import AnalysisPipeline
 from ..core import get_accuracy, get_per_label_accuracy
 
 from .utils import get_subject_loaded_pipelines
@@ -20,94 +20,105 @@ def accuracy_against_data_amount(
     model_names: list[str],
     training_options: dict[str, any],
     output_folder_path: str,
+    defer_subject_loading: bool = True
 ):
-    # Constants
     SUBAVERAGE_SIZE = 1
     NUM_FOLDS = 5
     TRIM_START_TIME = 50
     TRIM_END_TIME = 250
 
-    # Cached subject-loaded pipeline states
-    subject_loaded_pipelines = get_subject_loaded_pipelines(subject_filepaths)
-
-    def internal(subject_filepath, model_name):
-        results = []
-        labels = None
-        headers = ["Data Amount", "Accuracy"]
-        time_keeper = TimeKeeper()
-        durations = []
-        data_amounts = []
-
-        data_amount = min_trials
-        while data_amount < len(subject_loaded_pipelines[subject_filepath].subjects[0].trials):
-            data_amounts.append(data_amount)
-            base = subject_loaded_pipelines[subject_filepath]
-            p0 = PipelineState()
-            base.save(to=p0)
-            
-            p1 = (
-                p0
-                .trim_by_timestamp(start_time=TRIM_START_TIME, end_time=TRIM_END_TIME)
-            )
-
-            subject = p1.subjects[0]
-            trials = sample(deepcopy(subject.trials), data_amount)
-            for i, trial in enumerate(trials):
-                # Ensure that the trial's index matches its `trial_index` attribute
-                trial.trial_index = i
-            subject.trials = trials
-
-            _ = (
-                p1
-                .subaverage(size=SUBAVERAGE_SIZE)
-                .fold(num_folds=NUM_FOLDS)
-                .evaluate_model(
-                    model_name=model_name,
-                    training_options=training_options
-                )
-            )
-            if subject.trials[0].prediction is None:
-                continue
-            
-            plot_confusion_matrix(subject=subject, filepath=f"{output_folder_path}/{model_name}/{subject.name}/confusion/{data_amount}.svg")
-            plot_roc_curve(subject=subject, filepath=f"{output_folder_path}/{model_name}/{subject.name}/roc/{data_amount}.svg")
-
-            if labels is None:
-                labels = subject.labels_map.keys()
-                for label in labels:
-                    headers.append(f"Accuracy (label={label})")
-            
-            row_data = [data_amount, get_accuracy(subject)]
-            per_label_accuracies = get_per_label_accuracy(subject)
-            for label in labels:
-                row_data.append(per_label_accuracies[label])
-            
-            results.append(row_data)
-            t = time_keeper.lap_time()
-            durations.append(t)
-            print(f"{(t):.4f}s elapsed for data_amount = {data_amount}")
-            
-            data_amount += stride
-        
-        # Save the results
-        output_filepath = Path(output_folder_path) / model_name
-        output_filepath.mkdir(parents=True, exist_ok=True)
-        end = time_keeper.end_time()
-        _data_amounts = ["Data Amount"] + data_amounts + ["Total"]
-        _times = ["Time"] + durations + [end]
-        save_times(
-            _data_amounts, 
-            _times, 
-            output_filepath / f"{Path(subject_filepath).stem}.txt"
-        )
-        output_filepath = output_filepath / f"{Path(subject_filepath).stem}.csv"
-
-        df = pd.DataFrame(results, columns=headers)
-        df.to_csv(output_filepath, index=False)
-
-        print(f"{(end):.4f}s elapsed in total; results saved to: {output_filepath}")
-        return results
+    subject_loaded_pipelines = None
+    if not defer_subject_loading:
+        subject_loaded_pipelines = get_subject_loaded_pipelines(subject_filepaths)
 
     for model_name in model_names:
         for subject_filepath in subject_filepaths:
-            internal(subject_filepath=subject_filepath, model_name=model_name)
+            
+            data_amounts = []
+            headers = ["Data Amount", "Accuracy"]
+            accuracies = []
+            labels = None
+            time_keeper = TimeKeeper()
+            durations = []
+            
+            # The base subject pipeline state used for this subject; do not modify, only deeply copy
+            if not defer_subject_loading: 
+                subject_pipeline = subject_loaded_pipelines[subject_filepath]
+            else:
+                subject_pipeline = AnalysisPipeline().load_subjects(subject_filepath)
+            
+            data_amount = min_trials
+            while data_amount <= len(subject_pipeline.subjects[0].trials):
+                # Keep only `data_amount` number of trials
+                reduced_trials = sample(
+                    deepcopy(subject_pipeline.deepcopy().subjects[0].trials), 
+                    data_amount
+                )
+                
+                # Index them properly
+                for i, trial in enumerate(reduced_trials):
+                    trial.trial_index = i
+                
+                reduced_starting_pipeline = subject_pipeline.deepcopy()
+                reduced_starting_pipeline.subjects[0].trials = reduced_trials
+                
+                p = (
+                    reduced_starting_pipeline
+                    .trim_by_timestamp(start_time=TRIM_START_TIME, end_time=TRIM_END_TIME)
+                    .subaverage(size=SUBAVERAGE_SIZE)
+                    .fold(num_folds=NUM_FOLDS)
+                    .evaluate_model(
+                        model_name=model_name,
+                        training_options=training_options
+                    )
+                )
+                subject = p.subjects[0]
+                
+                # Ensure that labels and headers are arranged consistently for all subaverage sizes
+                # This is run only once
+                if labels is None:
+                    labels = subject.labels_map.keys()
+                    for label in labels:
+                        headers.append(f"Accuracy (label={label})")
+                
+                # Format the data for this iteration
+                row_data = [data_amount, get_accuracy(subject)]
+                per_label_accuracies = get_per_label_accuracy(subject)
+                for label in labels:
+                    row_data.append(per_label_accuracies[label])
+                    
+                accuracies.append(row_data)
+                t = time_keeper.lap_time()
+                durations.append(t)
+                print(f"{(t):.4f}s elapsed for {data_amount = }")
+                    
+                plot_confusion_matrix(
+                    subject=subject, 
+                    filepath=f"{output_folder_path}/{model_name}/{subject.name}/confusion/{data_amount}.svg"
+                )
+                plot_roc_curve(
+                    subject=subject, 
+                    filepath=f"{output_folder_path}/{model_name}/{subject.name}/roc/{data_amount}.svg"
+                )
+                
+                data_amounts.append(data_amount)
+                data_amount += stride
+            
+            # Save the results
+            output_filepath = Path(output_folder_path) / model_name
+            output_filepath.mkdir(parents=True, exist_ok=True)
+            end = time_keeper.end_time()
+            _data_amounts = ["Data Amount"] + data_amounts + ["Total"]
+            _times = ["Time"] + durations + [end]
+            
+            save_times(
+                _data_amounts, 
+                _times, 
+                output_filepath / f"{Path(subject_filepath).stem}.txt"
+            )
+            output_filepath = output_filepath / f"{Path(subject_filepath).stem}.csv"
+    
+            df = pd.DataFrame(accuracies, columns=headers)
+            df.to_csv(output_filepath, index=False)
+            print(f"{(end):.4f}s elapsed in total; results saved to: {output_filepath}")
+
