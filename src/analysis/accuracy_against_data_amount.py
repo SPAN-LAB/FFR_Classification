cd14edf3a9f594425dc4f05ebcdb808db818ef2f
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-02-10 16:45:57 -0600

    Update analysis to output JSON files only.

--- src/analysis/accuracy_against_data_amount.py
+++ src/analysis/accuracy_against_data_amount.py
@@ -9,18 +9,13 @@ Description: A function that evaluates a model's performance on various data amo
 
 
 from pathlib import Path
-import pandas as pd
+import json
 from copy import deepcopy
 from random import sample
 
 from ..core import AnalysisPipeline
-from ..core import get_accuracy, get_per_label_accuracy
 
 from .utils import get_subject_loaded_pipelines
-from .utils import save_times
-from ..time import TimeKeeper
-
-from ..core.plots import plot_confusion_matrix, plot_roc_curve
 
 
 def accuracy_against_data_amount(
@@ -43,13 +38,6 @@ def accuracy_against_data_amount(
 
     for model_name in model_names:
         for subject_filepath in subject_filepaths:
-            
-            data_amounts = []
-            headers = ["Data Amount", "Accuracy"]
-            accuracies = []
-            labels = None
-            time_keeper = TimeKeeper()
-            durations = []
             
             # The base subject pipeline state used for this subject; do not modify, only deeply copy
             if not defer_subject_loading: 
@@ -84,51 +72,20 @@ def accuracy_against_data_amount(
                 )
                 subject = p.subjects[0]
                 
-                # Ensure that labels and headers are arranged consistently for all subaverage sizes
-                # This is run only once
-                if labels is None:
-                    labels = subject.labels_map.keys()
-                    for label in labels:
-                        headers.append(f"Accuracy (label={label})")
+                # Create the dictionary
+                predictions = {"trials": []}
+                for trial in subject.trials:
+                    predictions["trials"].append({
+                        "label": trial.label,
+                        "prediction_distribution": trial.prediction_distribution
+                    })
                 
-                # Format the data for this iteration
-                row_data = [data_amount, get_accuracy(subject)]
-                per_label_accuracies = get_per_label_accuracy(subject)
-                for label in labels:
-                    row_data.append(per_label_accuracies[label])
-                    
-                accuracies.append(row_data)
-                t = time_keeper.lap_time()
-                durations.append(t)
-                print(f"{(t):.4f}s elapsed for {data_amount = }")
-                    
-                plot_confusion_matrix(
-                    subject=subject, 
-                    filepath=f"{output_folder_path}/{model_name}/{subject.name}/confusion/{data_amount}.svg"
-                )
-                plot_roc_curve(
-                    subject=subject, 
-                    filepath=f"{output_folder_path}/{model_name}/{subject.name}/roc/{data_amount}.svg"
-                )
+                # Save predictions to <output_dir_path>/<model-name>/<subject-name>/subaverage-<size>.json
+                path = Path(f"./{output_folder_path}/{model_name}/{Path(subject_filepath).stem}/data-amount-{data_amount}.json")
+                path.parent.mkdir(parents=True, exist_ok=True)
                 
-                data_amounts.append(data_amount)
+                with path.open("w", encoding="utf-8") as file:
+                    json.dump(predictions, file, indent=4)
+                    
                 data_amount += stride
-            
-            # Save the results
-            output_filepath = Path(output_folder_path) / model_name
-            output_filepath.mkdir(parents=True, exist_ok=True)
-            end = time_keeper.end_time()
-            _data_amounts = ["Data Amount"] + data_amounts + ["Total"]
-            _times = ["Time"] + durations + [end]
-            
-            save_times(
-                _data_amounts, 
-                _times, 
-                output_filepath / f"{Path(subject_filepath).stem}.txt"
-            )
-            output_filepath = output_filepath / f"{Path(subject_filepath).stem}.csv"
-    
-            df = pd.DataFrame(accuracies, columns=headers)
-            df.to_csv(output_filepath, index=False)
-            print(f"{(end):.4f}s elapsed in total; results saved to: {output_filepath}")
 
