41d7d66e4e71e040c822f649848dbc771c24ca76
shukla33 <shukla33@ap2001.chtc.wisc.edu>
2026-03-02 13:56:48 -0600

    minor changes

--- server_analysis/example.py
+++ server_analysis/example.py
@@ -2,5 +2,5 @@ from src.analysis import subaverage_size
 from src.analysis import data_amount
 
 
-subaverage_size.analyze("RNN")
-data_amount.analyze("RNN")
+subaverage_size.analyze("CNN")
+data_amount.analyze("CNN")
