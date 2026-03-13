5ae2359f8f69197dbe3905536b47c8be25ca0e56
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-03-13 10:35:22 -0500

    Improve the time keeper

--- src/time/time_keeper.py
+++ src/time/time_keeper.py
@@ -11,35 +11,45 @@ Description: A simple timer; useful for measuring how long some computation take
 import time
 
 class TimeKeeper:
-    def __init__(self):
-        self.start = time.perf_counter()
-        self.last = time.perf_counter()
+    def __init__(self, num_dps: int):
+        self.start_time = 0
+        self.last_resume_time = 0
+        self.last_lap_duration = 0
+        self.accumulated_duration = 0
+        self.reset()
+        self.num_dps = num_dps
+
+    def reset(self):
+        self.start_time = time.perf_counter()
+        self.last_resume_time = time.perf_counter()
+        self.last_lap_duration = 0
+        self.accumulated_duration = 0
+
+    def start(self):
+        self.start_time = time.perf_counter()
+        
+    def lap(self):
+        temp_last_lap_duration = time.perf_counter() - self.last_resume_time
+        if self.num_dps:
+            self.last_lap_duration = round(temp_last_lap_duration, self.num_dps)
+        else:
+            self.last_lap_duration = temp_last_lap_duration
+        
+        self.last_resume_time = time.perf_counter()
     
-    def lap_time(self) -> float:
-        """
-        Record a lap and get the time since the last lap.
-        """
-        curr_time = time.perf_counter() 
-        duration = curr_time - self.last
-        self.last = curr_time
-        return duration
-    
-    def end_time(self) -> float:
-        """
-        Get the time since the start of time-keeping.
-        """
-        return time.perf_counter() - self.start
-    
-    def peek_time(self) -> float:
-        return self.end_time()
+    def stop(self):
+        temp_accumulated_duration = time.perf_counter() - self.start_time
+        if self.num_dps:
+            self.accumulated_duration = round(temp_accumulated_duration, self.num_dps)
+        else:
+            self.accumulated_duration = temp_accumulated_duration
 
 if __name__ == "__main__": 
     
-    tk = TimeKeeper()
+    tk = TimeKeeper(num_dps=2)
     
-    for _ in range(1_000_000):
-        # ... some computation ...
-        print(f"Time since previous iteration: {tk.lap_time()} s")
-    
-    print(f"Total duration: {tk.end_time()}")
-    
+    tk.reset()
+    tk.start()
+    time.sleep(10)
+    tk.stop()
+    print(f"{tk.accumulated_duration}")
