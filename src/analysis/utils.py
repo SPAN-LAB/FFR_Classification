0534a4590224ea9c686b8c2b9c91b32f0b2788d9
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-03-13 11:42:48 -0500

    Squashed commit of the following:
    commit b2ab9da4f2b528d1828d5d13ced07f2714743090
    Author: Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
    Date:   Fri Mar 13 11:41:17 2026 -0500
    
        Refine analysis functions
    
        The purpose of this is to make adding logging and time-keeping eaiser.
    
    commit 600ebb9ebf240ecc8dc523b367f3d666ab46c711
    Author: Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
    Date:   Fri Mar 13 11:27:22 2026 -0500
    
        Create configurations.py

--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -7,11 +7,13 @@ Description: Utility functions used in the analysis code.
 """
 
 
+from copy import deepcopy
 import os
 from pathlib import Path
 import pickle
+from math import floor
 
-from ..core import AnalysisPipeline, PipelineState, EEGSubject
+from ..core import AnalysisPipeline, PipelineState, EEGSubject, EEGTrial
 from ..core.ffr_proc import get_accuracy
 
 
@@ -115,3 +117,66 @@ def get_results(dir_path: str) -> list[tuple[int, float]]:
     
     axes.sort(key=lambda x: x[0])
     return axes
+
+def stratified_deterministic_sample(subject: EEGSubject, num_trials: int) -> [EEGTrial]:
+    """
+    Performs a stratified sample on the trials of the EEGSubject deterministically. 
+    In other words, identical inputs to this function will provide identical outputs, and there 
+    is no randomization involved. 
+    """
+    
+    total_num_trials = len(subject.trials)
+    grouped_trials = deepcopy(subject).grouped_trials()
+    
+    # Determine the number of trials each class needs 
+    
+    # Keys are any (all possible labels/classes of the subject).
+    # Values are 2-element tuples, each of which represents the number of trials
+    # this class should have, given the number of trials passed to this func.
+    # The second element is the floored result of the first.
+    num_trials_per_label = {}
+    for label, trials in grouped_trials.items():
+        n = len(trials) / total_num_trials * num_trials
+        num_trials_per_label[label] = (n, floor(n))
+        
+    # Determine the distance between the unfloored and floored values
+    num_trials_allocated = 0
+    distances = []
+    for label, num_trials_tuple in num_trials_per_label.items():
+        distances.append((label, num_trials_tuple[0] - num_trials_tuple[1]))
+        num_trials_allocated += num_trials_tuple[1]
+    distances.sort(key=lambda x: x[1], reverse=True)
+    
+    # Now, distances contains tuples of the form (Label, fractional_trials_needed)
+    # sorted by the second value. So we can allocate remaining trials to 
+    # classes whose priority is highest
+    
+    for distance in distances:
+        if num_trials_allocated >= num_trials:
+            break
+        else:
+            num_trials_per_label[distance[0]][1] += 1
+            num_trials_allocated += 1
+    
+    if num_trials_allocated != num_trials:
+        raise ValueError("Failed sample exactly the specified number of trials")
+    
+    # Now, the second element in the tuples (values) of num_trials_per_label 
+    # corresponds to the number of trials we're going to use for that class
+    
+    trials = []
+    for label, trials in grouped_trials.items():
+        num_trials_to_take = num_trials_per_label[label][1]
+        trials += grouped_trials[:num_trials_to_take]
+    
+    # Reassign indices to the trials 
+    for i, trial in enumerate(trials):
+        trial.trial_index = i
+    
+    return trials
+
+def strip_data_away(subject: EEGSubject):
+    for trial in subject.trials:
+        trial.data = []
+        trial.timestamps = []
+    subject.folds = []
