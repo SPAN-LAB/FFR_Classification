d46190ce24e116bdcec5f7ad569ac7db084b01e2
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-03-14 15:40:04 -0500

    Improve the TimeKeeper

--- src/time/time_keeper.py
+++ src/time/time_keeper.py
@@ -11,7 +11,7 @@ Description: A simple timer; useful for measuring how long some computation take
 import time
 
 class TimeKeeper:
-    def __init__(self, num_dps: int):
+    def __init__(self, num_dps: int | None = 3):
         self.start_time = 0
         self.last_resume_time = 0
         self.last_lap_duration = 0
@@ -29,13 +29,30 @@ class TimeKeeper:
         self.start_time = time.perf_counter()
         
     def lap(self):
+        
+        # Lap duration
         temp_last_lap_duration = time.perf_counter() - self.last_resume_time
         if self.num_dps:
             self.last_lap_duration = round(temp_last_lap_duration, self.num_dps)
         else:
             self.last_lap_duration = temp_last_lap_duration
+            
+        # Full duration (from the start)
+        temp_accumulated_duration = time.perf_counter() - self.start_time
+        if self.num_dps:
+            self.accumulated_duration = round(temp_accumulated_duration, self.num_dps)
+        else:
+            self.accumulated_duration = temp_accumulated_duration
         
         self.last_resume_time = time.perf_counter()
+    
+    def lap_and_get(self) -> float:
+        self.lap()
+        return self.last_lap_duration
+    
+    def stop_and_get(self) -> float:
+        self.stop
+        return self.accumulated_duration
     
     def stop(self):
         temp_accumulated_duration = time.perf_counter() - self.start_time
