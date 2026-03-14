2d8b469f51debcd6a92addf360251717ba31e598
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-03-14 15:40:53 -0500

    Add training options to configurations
    Good for defaults

--- src/configurations.py
+++ src/configurations.py
@@ -1,6 +1,12 @@
 # DEFAULTS
 
-SUBAVERAGE_SIZE = 5
-NUM_FOLDS = 5
-TRIM_START_TIME = 50
-TRIM_END_TIME = 250
+SUBAVERAGE_SIZE  = 5
+NUM_FOLDS        = 5
+TRIM_START_TIME  = 50  # in milliseconds
+TRIM_END_TIME    = 250 # in milliseconds
+TRAINING_OPTIONS = {
+    "num_epochs": 20,
+    "batch_size": 32,
+    "learning_rate": 0.001,
+    "weight_decay": 0.1
+}
