24d47e3e33e19f60ea01a2ed7d87038456b0c22b
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-03-14 15:40:24 -0500

    Add is_empty function to logging

--- src/printing/logging.py
+++ src/printing/logging.py
@@ -14,6 +14,25 @@ def log(content, path: str | Path):
     path.parent.mkdir(parents=True, exist_ok=True)
     with open(path, "a") as file:
         if path.stat().st_size > 0:
-            file.write("\n" + content)
+            file.write("\n" + f"{content}")
         else:
-            file.write(content)
+            file.write(f"{content}")
+
+def is_empty(path: str | Path):
+    """
+    Checks if the file referenced by path does not have any content, 
+    returning true when the above statement is true.
+    """
+    
+    if isinstance(path, str):
+        path = Path(path)
+    
+    if not path.exists():
+        raise ValueError("Path does not exist.")
+    
+    if path.is_dir():
+        raise ValueError("Path points to directory; should be file.")
+    
+    if path.stat().st_size > 0:
+        return False
+    return True
