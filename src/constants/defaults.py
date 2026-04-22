d4d9606d69c712253deaaed8f4a5437e8ccbe6d0
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-04-22 11:45:38 -0500

    Update defaults

--- src/constants/defaults.py
+++ src/constants/defaults.py
@@ -1,6 +1,6 @@
 NUM_EPOCHS = 20
-BATCH_SIZE = 10_000
-LEARNING_RATE = 0.001
+BATCH_SIZE = 32
+LEARNING_RATE = 0.0001
 WEIGHT_DECAY = 0.1
 MIN_DELTA = 0.0001
 PATIENCE = 10
@@ -12,8 +12,12 @@ NUM_FOLDS        = 5
 TRIM_START_TIME  = 50  # in milliseconds
 TRIM_END_TIME    = 250 # in milliseconds
 TRAINING_OPTIONS = {
-    "num_epochs": 20,
-    "batch_size": 32,
-    "learning_rate": 0.001,
-    "weight_decay": 0.1
+    "num_epochs": NUM_EPOCHS,
+    "batch_size": BATCH_SIZE,
+    "learning_rate": LEARNING_RATE,
+    "weight_decay": LEARNING_RATE,
+    "min_delta": MIN_DELTA,
+    "patience": PATIENCE,
+    "lr_patience": LR_PATIENCE,
+    "validation_ratio": VALIDATION_RATIO
 }
