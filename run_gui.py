01b5c060be0e341bc75186ebeeb4e35505c4356a
Chiranjeevi Vasant <chiranjeevivasant@Chiranjeevis-MacBook-Pro.local>
2026-07-03 12:26:46 -0500

    feat: add run_gui.py, update GUI files with latest fixes

---
+++ run_gui.py
@@ -0,0 +1,13 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+try:
+    import torch
+except Exception:
+    pass
+
+from src.GUI.gui import main
+
+if __name__ == "__main__":
+    main()
